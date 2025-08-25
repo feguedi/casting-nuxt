4eec4733479050094bf50f37ac43d3c3f34c7fbb
Admin <admin@mondragonmexico.edu.mx>
2025-08-25 10:18:34 -0600

    Agregadas configuraciones y módulos de Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      charset: 'UTF-8',
+      title: 'Casting Carreras Profesionales: Registro | Universidad Mondragón México',
+    },
+  },
+
   modules: [
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
+    'nuxt-schema-org',
+    'nuxt-og-image',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/scripts'
-  ]
-})
+    '@nuxt/scripts',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@weareheavy/nuxt-cookie-consent',
+  ],
+});
